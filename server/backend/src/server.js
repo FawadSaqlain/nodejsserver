5d5b15b595a9d4c3202dbd20069044ff7be368e7
fawad saqlain <saqlainfawad@gmail.com>
2025-05-30 10:57:53 +0500

    changes for deploying now try again start

--- server/backend/src/server.js
+++
@@ -1,7 +0,0 @@
-// src/server.js (local dev only)
-const app = require('./app');
-const { PORT = 3000 } = process.env;
-
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
